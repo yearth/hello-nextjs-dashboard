7fb403ce2902d76a82d11c460c78a405627b5536
yearth <yearthmain@gmail.com>
2024-08-05 23:24:58 +0800

    feat: add images on desktop

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 import Link from "next/link";
 import AcmeLogo from "./ui/acme-logo";
 import { lusitana } from "./ui/fonts";
@@ -30,6 +31,13 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
